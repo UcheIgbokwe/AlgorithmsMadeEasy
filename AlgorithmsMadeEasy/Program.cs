3a0b08a935151ae24c5d3fefb9506c0b5bb9b8f3
UcheIgbokwe <uchechukwu.igbokwe@zenithbank.com>
2020-07-12 12:16:55 +0100

    Longest Common Prefix

--- AlgorithmsMadeEasy/Program.cs
+++ AlgorithmsMadeEasy/Program.cs
@@ -6,7 +6,76 @@ namespace AlgorithmsMadeEasy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region LongestPrefix
+
+            string[] sampleWords = new string[3] { "fly", "flask", "fletcher" };
+            var longestPrefix = Program.LongestPrefix(sampleWords);
+
+            Console.WriteLine($"LongestPrefix = {longestPrefix}");
+            #endregion
+
+
+            //Console.WriteLine("Hello World!");
+        }
+
+        #region UcheIgbokwe
+        //20200712
+        //Write a function to find the longest common prefix string amongst an array of strings. If there is no common prefix, return an empty string "".
+        #endregion
+        public static string LongestPrefix(string[] longWords)
+        {
+            string longestPrefix = string.Empty;
+
+            if (longWords.Length == 0)
+            {
+                longestPrefix = "";
+            }
+            else
+            {
+                int numberOfWords = longWords.Length;
+
+                for (int i = 0; i < numberOfWords; i++)
+                {
+                    string[] selectedLetter = new string[numberOfWords];
+                    int wordsPicked = 1;
+                    int matchingLetters = 0;
+
+                    //add the character to an array.
+                    selectedLetter[0] = longWords[0].Substring(i, 1);
+
+                    //This will pick the words after the first word and extract their sepecified character.
+                    while (wordsPicked < numberOfWords)
+                    {
+                        //add the character to an array.
+                        selectedLetter[wordsPicked] = longWords[wordsPicked].Substring(i, 1);
+                        wordsPicked++;
+                    }
+
+                    //Check if each letter picked from each word matches.
+                    if (selectedLetter.Length > 0)
+                    {
+                        for (int j = 0; j < selectedLetter.Length; j++)
+                        {
+                            if (selectedLetter[0] == selectedLetter[j])
+                            {
+                                //check if all the characters selected are equal.
+                                matchingLetters += 1;
+                            }
+                        }
+
+                        //if the number of words match the number of characters selected, then it's a yes.
+                        if (matchingLetters == numberOfWords)
+                        {
+                            longestPrefix += selectedLetter[0];
+
+                        }
+                    }
+
+                }
+
+
+            }
+            return longestPrefix;
         }
     }
 }
